b1056bf321aa32e94bfb251aae2450106648bed2
tweoss <francischua689@gmail.com>
2022-10-19 16:20:24 -0700

    additional explanation in comments

--- inclusion_exclusion/inc_exc/src/main.rs
+++ inclusion_exclusion/inc_exc/src/main.rs
@@ -21,7 +21,6 @@
 use std::{
     collections::{HashMap, VecDeque},
     fs,
-    path::PathBuf,
     time::Instant,
 };
 
@@ -84,15 +83,22 @@ fn merge(a: Clause, b: &Clause) -> Merge {
 fn solve(dnf: &DNF, max_size: usize) -> (SolutionResult, usize, i64) {
     // a map from a set of clause indices to their merged set if it is valid
     let mut cache: HashMap<Vec<usize>, Clause> = HashMap::new();
+    // 0th generation. the empty set of clauses has no literals in its merge
     cache.insert(vec![], HashMap::new());
+    // 0th generation. no combination of indices yet
     let mut queue: VecDeque<Vec<usize>> = VecDeque::from(vec![vec![]]);
+    // critical variable for testing for completion
     let mut sum = 0;
+    // total number of variables in all the clauses. (alternatively, parse from the dimacs input)
     let total_vars = dnf
         .iter()
         .map(|h| h.iter())
         .flat_map(|i| i)
         .collect::<HashMap<_, _>>()
         .len() as u32;
+    // the queue can contain future and current generations (generations by size of subset)
+    // therefore, we use max_seen_size to determine when we have finished one generation 
+    // and will start processing the next generation
     let mut max_seen_size = 0;
     while let Some(set) = queue.pop_front() {
         // remove from cache the merged set for this clause set.
@@ -102,6 +108,8 @@ fn solve(dnf: &DNF, max_size: usize) -> (SolutionResult, usize, i64) {
         if set.len() != max_size {
             // extend by another index
             // this new index must be greater than the last index in the set
+            // because our set is an ordered list of increasing indices, this guarantees 
+            // every new set we create will be unique
             for new_index in (set.last().map(|&e| e + 1).unwrap_or(0))..(dnf.len()) {
                 // merge the cached set with the clause at the new_index
                 match merge(cache_entry.clone(), &dnf[new_index]) {
@@ -119,7 +127,7 @@ fn solve(dnf: &DNF, max_size: usize) -> (SolutionResult, usize, i64) {
                     }
                     // if incompatible, we do not wish to further explore this and thus do not insert into queue
                     // HOWEVER, it may be wise to modify cache structure to allow storing INVALID results
-                    // we may explore 1,3 before 1,2,3. if 1,3 is invalid, we may store that it is invalid and look up later
+                    // for example, we may explore 1,3 before 1,2,3. if 1,3 is invalid, we may store that it is invalid and look up later
                     // however, 1,3 is not a direct ancestor of 1,2,3. thus, this search would necessitate linear probing of
                     // every possible indirect ancestor in the previous generation: 2,3; 1,3; 1,2
                     // thus, we do not implement it here
@@ -131,9 +139,9 @@ fn solve(dnf: &DNF, max_size: usize) -> (SolutionResult, usize, i64) {
             return (SolutionResult::Inconclusive, max_seen_size, sum);
         }
         // if this new length is greater than all previous seen sizes, we must have
-        // reached a new level of lengths
+        // reached a new level of lengths (advanced to next generation)
         // by nature of the queue logic, the sizes must be always nondecreasing
-        // therefore, once we "level up", we may analyze the previous level of sizes
+        // therefore, once we "level up", we may analyze the sum, as it now accounts for the entire previous level of sizes
         if set.len() > max_seen_size {
             // check the sum and see if it is (a lower bound and above 2^N) or (an upper bound and below 2^n)
             if max_seen_size % 2 == 0 && sum >= i64::pow(2, total_vars) {
